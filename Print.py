742d8cc37738b5abe3e8e8198f4a0aae020434e6
EasyBreadToast <66406642+EasyBreadToast@users.noreply.github.com>
2020-07-28 14:19:22 -0700

    Update Print.py

--- Print.py
+++ Print.py
@@ -1,15 +1,16 @@
 import pyautogui 
 import time
 
+
 while True: 
     f = pyautogui.locateCenterOnScreen('Untitled.png', confidence=0.8)   
-    if f != None:
+    if f != None:              #If it founds it....
         print("Found")
         pyautogui.click(f)
         time.sleep(1)
         break
     else:
-        print("Not found, relocating...")
+        print("Not found, relocating...") #Will keep finding it...
         time.sleep(1)
 
 print('Image located... stopped')
